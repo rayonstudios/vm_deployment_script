64b1fd836038cceb54ede782fef4edf6ccb75fd1
MohammedMaaz <maazkhawar25@gmail.com>
2024-05-24 04:43:20 +0500

    specify user

--- startup.sh
+++ startup.sh
@@ -2,6 +2,7 @@ set -v
 
 REPO_URL="https://github.com/org/repo"
 NODE_VERSION="20.13.1"
+USER="thetrendshakers"
 
 # Install logging monitor. The monitor will automatically pick up logs sent to
 # syslog.
@@ -11,14 +12,7 @@ service google-fluentd restart &
 
 # Install dependencies from apt
 apt-get update
-apt-get install -yq ca-certificates git build-essential supervisor
-
-# Install nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-source ~/.bashrc
-
-# Install nodejs
-nvm install $NODE_VERSION
+apt-get install -yq ca-certificates git build-essential
 
 # Clone vm_deployment_script repo
 # git requires $HOME and it's not set during the startup script.
@@ -28,27 +22,26 @@ rm -rf /opt/app/vm_deployment_script
 git clone "https://github.com/rayonstudios/vm_deployment_script.git" /opt/app/vm_deployment_script
 cd /opt/app/vm_deployment_script
 
+# Install nvm
+sudo -u $USER curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+sudo -u $USER source ~/.bashrc
+
+# Install nodejs
+sudo -u $USER nvm install $NODE_VERSION
+
 # Create .env with REPO_URL secret
 echo "REPO_URL=$REPO_URL" > ./.env
 
 # Install app dependencies
-npm install
-
-# Create a nodeapp user. The application will run as this user.
-useradd -m -d /home/nodeapp nodeapp
-chown -R nodeapp:nodeapp /opt/app
+sudo -u $USER npm install
 
 # Install pm2
-npm install -g pm2@5.3.1
+sudo -u $USER npm install -g pm2@5.3.1
 
 # build
-npm run build
+sudo -u $USER npm run build
 
-pm2 stop vm_deployment_script
-pm2 start ./dist/server.js --name vm_deployment_script
-
-supervisorctl reread
-supervisorctl update
+sudo -u $USER pm2 start -f ./dist/server.js --name vm_deployment_script
 
 # run the apps for the first time
 curl http://localhost:3001/deploy
